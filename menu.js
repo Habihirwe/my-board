d07580fd340bf7b09dd7aacc5a9f420059daf354
Habihirwe_Olivier <habiholivier10@gmail.com>
2022-11-30 14:41:27 -0800

    initial commit

---
+++ menu.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector(".menu");
+let navbar = document.querySelector('.list');
+
+menu.onclick = ()=> {
+    // menu.classList.toggle('');
+    navbar.classList.toggle('open')
+}
+
+
+
+
+
+
+
+
+
+
+
